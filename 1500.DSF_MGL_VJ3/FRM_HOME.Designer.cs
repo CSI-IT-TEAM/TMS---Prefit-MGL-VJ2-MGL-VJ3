51b27da104080f227047cc46c1df05e5abb22b0d
PHUOC.IT <PHUOC.IT@VJ-V-IT-PHUOC>
2021-01-23 14:10:15 +0700

    commit

--- 1500.DSF_MGL_VJ3/FRM_HOME.Designer.cs
+++ 1500.DSF_MGL_VJ3/FRM_HOME.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.tblMain = new System.Windows.Forms.TableLayoutPanel();
             this.pnHeader = new System.Windows.Forms.Panel();
             this.btnUpstream = new System.Windows.Forms.Button();
@@ -38,7 +37,7 @@
             this.splContent = new System.Windows.Forms.SplitContainer();
             this.tblContent = new System.Windows.Forms.TableLayoutPanel();
             this.tblChart = new System.Windows.Forms.TableLayoutPanel();
-            this.tmrDate = new System.Windows.Forms.Timer(this.components);
+            this.tmrDate = new System.Windows.Forms.Timer();
             this.tblMain.SuspendLayout();
             this.pnHeader.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splContent)).BeginInit();
